45dd78e8442d560b0bc67dfcdbc70b91018e2c9e
jeremycryan <jeremychristopherryan@gmail.com>
2019-11-14 12:37:51 -0500

    Add controller class, generalize visualization

--- constants.py
+++ constants.py
@@ -4,3 +4,7 @@ DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 DIRECTIONS = (UP, DOWN, LEFT, RIGHT)
+
+EMPTY_TILE = "."
+TAIL_TILE = "+"
+PLAYER_TILE = "O"
--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+import time
+import sys
 
 import controller
 
@@ -11,7 +13,8 @@ player_count = 1
 class Game(object):
 	def __init__(self):
 		pygame.init()
-		self.board = [[0 for i in range(board_dims[1]-2)] for j in range(board_dims[0]-2)]
+		self.screen = pygame.display.set_mode((100, 100))
+		self.board = [[EMPTY_TILE for i in range(board_dims[1]-2)] for j in range(board_dims[0]-2)]
 
 		self.players = []
 		for i in range(player_count):
@@ -22,29 +25,37 @@ class Game(object):
 
 	def main(self):
 		while self.players:
-			pygame.event.pump()
+			events = pygame.event.get()
+			self.check_close(events)
 
 			for i in range(len(self.board[0])):
 				for j in range(len(self.board)):
 					print(self.board[j][i], end=' ')
 				print()
+			print()
 			for i in range(len(self.players)-1, -1, -1):
 				player = self.players[i]
-				self.board[player.x][player.y] = 1
+				self.board[player.x][player.y] = TAIL_TILE
 				move = player.move()
-				#print(move)
-				if (self.board[player.x][player.y] == 0):
-					self.board[player.x][player.y] = 2
+				if (self.board[player.x][player.y] == EMPTY_TILE):
+					self.board[player.x][player.y] = PLAYER_TILE
 				else:
 					player.die()
 					del self.players[i]
+			time.sleep(0.25)
+
+	def check_close(self, events):
+		for event in events:
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
 
 class Player(object):
 	def __init__(self, x, y, game):
 		self.x = x
 		self.y = y
 		self.game = game
-		self.game.board[x][y] = 2
+		self.game.board[x][y] = PLAYER_TILE
 		self.controller = controller.KeyboardController()
 
 	def move(self):
