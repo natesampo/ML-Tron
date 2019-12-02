4890c04f0e31002c9286ae4bb412d47d569a38ba
jeremycryan <jeremychristopherryan@gmail.com>
2019-12-02 12:10:00 -0500

    Small adjustments to edge and node; add fully connected method to Agent

--- nn_tools.py
+++ nn_tools.py
@@ -1,14 +1,15 @@
 import math
+import random
 
 
 class Node:
 
-    def __init__(self, innovation, bias, edges_in=[], edges_out=[], activation_func=None):
+    def __init__(self, innovation, bias=None, edges_in=None, edges_out=None, activation_func=None):
         self.val = None
         self.number = innovation
-        self.bias = bias
-        self.edges_in = edges_in
-        self.edges_out = edges_out
+        self.bias = bias if bias else self.random_bias()
+        self.edges_in = edges_in if edges_in else []
+        self.edges_out = edges_out if edges_out else []
         self.activation_func = self.sigmoid if not activation_func else activation_func
 
     def get_input_sum(self):
@@ -31,6 +32,16 @@ class Node:
         """ Resets the stored value. """
         self.val = None
 
+    def __repr__(self):
+        return f"\nNode (I={self.number}, " \
+               f"in={sorted([item.in_node.number for item in self.edges_in])}, " \
+               f"out={sorted([item.out_node.number for item in self.edges_out])}"
+
+    @staticmethod
+    def random_bias():
+        """ Returns a random value for a bias term. """
+        return random.gauss(0, 1)
+
     @staticmethod
     def sigmoid(x):
         return 1 / (1 + math.exp(-x))
@@ -38,12 +49,15 @@ class Node:
 
 class Edge:
 
-    def __init__(self, in_node, out_node, weight, enabled, innovation):
+    def __init__(self, innovation, in_node, out_node, weight=None, enabled=True):
         self.in_node = in_node
         self.out_node = out_node
-        self.weight = weight
+        self.weight = weight if weight else self.random_weight()
         self.enabled = enabled
         self.innovation = innovation
+
+        in_node.edges_out.append(self)
+        out_node.edges_in.append(self)
 
     def get_output_value(self):
         """ Returns the output value based on weight, input node,
@@ -54,6 +68,11 @@ class Edge:
         else:
             return 0
 
+    @staticmethod
+    def random_weight():
+        """ Returns a random weight. """
+        return random.gauss(0, 1)
+
 
 class Agent:
 
@@ -61,7 +80,38 @@ class Agent:
         self.pop = population
         self.nodes = set()
         self.input_nodes = set()
+        self.output_nodes = set()
         self.edges = set()
+
+    def create_fully_connected(self, *args):
+        """ Creates a fully connected neural network, with the input values being the number of nodes in each layer.
+
+            For instance, create_fully_connected(3, 4, 10) creates a model with three input nodes, four intermediary
+            nodes, and ten output nodes.
+        """
+        self.nodes = set()
+        self.edges = set()
+        self.input_nodes = set()
+
+        previous_layer = None
+        for layer in args:
+            new_nodes = set(Node(innovation=self.pop.new_node_number()) for _ in range(layer))
+
+            # Connect edges from previous layer to new layer
+            if previous_layer is not None:
+                for node_1 in previous_layer:
+                    for node_2 in new_nodes:
+                        self.edges.add(Edge(self.pop.new_innovation_number(), node_1, node_2))
+
+            # The first layer is the input nodes
+            else:
+                self.input_nodes = new_nodes
+
+            previous_layer = new_nodes
+            self.nodes |= new_nodes
+
+        # The last layer is output nodes
+        self.output_nodes = previous_layer
 
     # TODO write methods for adding nodes and edges
     # TODO make reasonable way to initialize and give input states
@@ -97,4 +147,3 @@ class Population:
 
     # TODO add population simulation
     # TODO program ability to add nodes
-
